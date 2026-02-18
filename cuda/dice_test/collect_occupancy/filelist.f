d3392a3c83f1f5fd5ecd583dfce1f277ff08441a
Jiayi Wang <jwang710@uw.edu>
2026-02-17 22:54:21 -0800

    update before modify

--- cuda/dice_test/collect_occupancy/filelist.f
+++ cuda/dice_test/collect_occupancy/filelist.f
@@ -1,7 +1,7 @@
-/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/nn_cuda/test_dice_20250808_141222.log
-/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/bfs/test_dice_20250808_230308.log
-/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/backprop/test_dice_20250808_233229.log
-/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/streamcluster/test_dice_20250808_234629.log
-/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/gaussian/test_dice_20250809_002554.log
-/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/hotspot/test_dice_20250809_013025.log
-/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/pathfinder/test_dice_20250809_005904.log
+/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/nn_cuda/test_dice_20250908_215357.log
+/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/bfs/test_dice_20250908_220401.log
+/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/backprop/test_dice_20250910_211343.log
+/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/streamcluster/test_dice_20250915_203416.log
+/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/gaussian/test_dice_20250910_221824.log
+/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/hotspot/test_dice_20250915_132018.log
+/data/jwang710/dice-test-gpu-rodinia/cuda/dice_test/result_summary/pathfinder/test_dice_20250912_011213.log
